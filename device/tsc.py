bc56f253614f59dc1e9f3627bdd4e8c076d565f0
Aoyaga <Aoyaga@LT-ES-PF571D1J>
2026-04-27 13:19:59 +0200

    Abort due to retries correctly

--- device/tsc.py
+++ device/tsc.py
@@ -152,8 +152,8 @@ class Tsc(Client):
                     self.gateway_ip, self.id, e, self.get_iteration()))
 
 
-            if self.iteration > max_iterations_per_device:
-                self.log.error(
-                    "GW {}, TSC {}: Too many retries. Aborting update process.".format(
-                        self.gateway_ip, self.id))
-                self.set_aborted(True)
+        if self.iteration >= max_iterations_per_device:
+            self.log.error(
+                "GW {}, TSC {}: Too many retries. Aborting update process.".format(
+                    self.gateway_ip, self.id))
+            self.set_aborted(True)
